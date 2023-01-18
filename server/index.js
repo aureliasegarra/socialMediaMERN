39d33d7a9df77ea5a52ef90e7c8bd54d771733f2
Aurelia Segarra <av.segarra@gmail.com>
2023-01-18 11:59:06 +0100

    Update env variables

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -10,11 +11,11 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://aure:aure123%40@clusteraurelia.mdzcepl.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION = process.env.CONNECTION_URL; 
 const PORT = process.env.PORT || 5000;
 
 mongoose.set('strictQuery', false);
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true })
+mongoose.connect(CONNECTION, { useNewUrlParser: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
 
